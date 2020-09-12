486f4dae929c9eebc3bef965e31107d50b9f84e6
juancarlosflorespucho <florespuchojuan@outlook.com>
2020-09-12 04:45:45 -0500

    agregamos nuesvas interfaces y los modules node para compilar fast

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,20 @@ router.get('/', (req, res) => {
     res.render('index.html');
 });
 
+router.get('/descripcion', (req, res) => {
+    res.render('descripcion.html');
+});
+
+router.get('/producto', (req, res) => {
+    res.render('productos.html');
+});
+
+router.get('/carrito', (req, res) => {
+    res.render('carrito.html');
+});
+
+router.get('/detallesEnvio', (req, res) => {
+    res.render('datosEntrega.html');
+});
+
 module.exports = router;
